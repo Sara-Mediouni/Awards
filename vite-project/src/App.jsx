708489d34d8e41d3dc8c89b3c811f482c3f6c796
Sara Mediouni <81996794+Sara-Mediouni@users.noreply.github.com>
2025-03-23 22:19:13 +0100

    fifth commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import NavBar from "./components/NavBar"
+import Features from "./components/Features"
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -9,6 +10,7 @@ const App = () => {
     <NavBar/>
     <Hero/>
     <About/>
+    <Features/>
     </main>
   )
 }
